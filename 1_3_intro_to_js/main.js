bfe29da7f3c9f00c5c9a7100fac1ffcc192e6810
gulya <gulya.radjapova@gmail.com>
2021-09-22 16:54:33 -0400

    buttons

--- 1_3_intro_to_js/main.js
+++ 1_3_intro_to_js/main.js
@@ -2,10 +2,21 @@ const btn = document.querySelector('.btn');
 btn.onclick = Counter;
 const clicks = document.querySelector('.clicks');
 clicks.id = document.querySelector('clicks');
+const label = document.getElementById("name-label")
+const submit = document.getElementById("name-submit")
 
 var button_clicks = 0;
+
 
 function Counter() {
     button_clicks += 1;
     clicks.innerHTML = button_clicks;
+}
+
+function updateName() {
+  const username = document.getElementById("name-input").value
+  // window.alert("Hello " + document.getElementById("name-input").value + ",welcome to class")
+//   window.alert(`Hello ${username}, welcome to class`)
+  label.innerText = `Your name is ${username}. Change it here:`
+  submit.innerText = "Change"
 }
