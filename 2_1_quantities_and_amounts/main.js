6c1f5edf2b3780f97180d4f95ee9e638b0f2fd86
gulya <gulya.radjapova@gmail.com>
2021-10-19 17:59:25 -0400

    2-1

--- 2_1_quantities_and_amounts/main.js
+++ 2_1_quantities_and_amounts/main.js
@@ -1,17 +1,53 @@
-/* CONSTANTS AND GLOBALS */
-// const width = ;
-// const height = ;
+// set the dimensions and margins of the graph
+var margin = {top: 20, right: 30, bottom: 40, left: 150},
+    width = 700 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
-/* LOAD DATA */
-d3.csv('../[PATH_TO_YOUR_DATA]', d3.autoType)
-  .then(data => {
-    console.log("data", data)
+// append the svg object to the body of the page
+var svg = d3.select("#container")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
 
-    /* SCALES */
-    /** This is where you should define your scales from data to pixel space */
-    
+// Parse the Data
+d3.csv("../data/peloton_workouts.csv", function(data) {
 
-    /* HTML ELEMENTS */
-    /** Select your container and append the visual elements to it */
+  // Add X axis
+  var x = d3.scaleLinear()
+    .domain([0, 4000])
+    .range([ 0, width]);
 
-  })
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x))
+    .selectAll("text")
+      .attr("transform", "translate(-10,0)rotate(-45)")
+      .style("text-anchor", "end")
+      .style("font-family", "Noto Sans Mono");
+
+  // Y axis
+  var y = d3.scaleBand()
+    .range([ 0, height ])
+    .domain(data.map(function(d) { return d.Instructor; }))
+    .padding(.15);
+
+  svg.append("g")
+    .call(d3.axisLeft(y))
+    .style("font-family", "Noto Sans Mono");
+
+  //Bars
+  svg.selectAll(".myRect")
+    .data(data)
+    .enter()
+    .append("rect")
+    .attr("x", x(0) )
+    .attr("y", function(d) { return y(d.Instructor); })
+    .attr("width", function(d) { return x(d.Minutes); })
+    .attr("height", y.bandwidth() )
+    .attr("fill", "#ffcccc")
+    .style("font-family", "Noto Sans Mono");
+
+})
